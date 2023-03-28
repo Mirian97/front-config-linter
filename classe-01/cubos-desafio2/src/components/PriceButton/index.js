d0421f163d89c96e2e0866a6d5b9dca9f16d3eac
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2023-03-28 18:52:10 -0300

    configuração do linter

---
+++ classe-01/cubos-desafio2/src/components/PriceButton/index.js
@@ -0,0 +1,12 @@
+import './styles.css';
+
+function PriceButton({ onClickHandler, text, price, type }) {
+  return (
+    <button className={"price-button " + type} onClick={onClickHandler}>
+      {text}
+      <span className="price">R$ {price.toString().replace('.', ',')}</span>
+    </button>
+  );
+}
+
+export default PriceButton;
